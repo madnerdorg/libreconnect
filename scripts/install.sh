1f81796b791fe066cb6753344b8302fea2c25f05
maditnerd <remi@madnerd.org>
2017-07-03 20:19:25 +0200

    [scripts] Update scripts for Raspberry Pi install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+apt-get install pip
 pip install pyserial
 pip install twisted
 pip install autobahn
 pip install service_identity
+pip install urlparse2
+pip install websocket-client
+pip install pyinstaller
+
+#Fix for TLSVersion.TLSv1_1: SSL.OP_NO_TLSv1_1
+
+apt-get remove python-openssl
+pip install pyopenssl
